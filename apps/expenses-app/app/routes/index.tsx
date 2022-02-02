7000955050832e31195a9c0e0c83c27762faac01
Santiago Martín Agra <santiagomartinagra@gmail.com>
2022-02-02 11:34:27 +0100

    chore: improve lint commands

--- apps/expenses-app/app/routes/index.tsx
+++ apps/expenses-app/app/routes/index.tsx
@@ -1,25 +1,25 @@
-import { ExpenseGroup, GetAllExpenseGroupsUseCase } from "expenses-app-domain";
-import { InMemoryExpenseGroupRepository } from "expenses-app-data";
-import { useLoaderData } from "remix";
+import { InMemoryExpenseGroupRepository } from 'expenses-app-data';
+import { ExpenseGroup, GetAllExpenseGroupsUseCase } from 'expenses-app-domain';
+import { useLoaderData } from 'remix';
 
 export async function loader(): Promise<ExpenseGroup[]> {
-  const { data, error } = await new GetAllExpenseGroupsUseCase(
-    new InMemoryExpenseGroupRepository()
-  ).perform();
+	const { data, error } = await new GetAllExpenseGroupsUseCase(
+		new InMemoryExpenseGroupRepository(),
+	).perform();
 
-  if (error) {
-    throw new Error(`[${error.type}] ${error.message}`);
-  }
+	if (error) {
+		throw new Error(`[${error.type}] ${error.message}`);
+	}
 
-  return data || [];
+	return data || [];
 }
 
 export default function Index() {
-  const expenseGroups = useLoaderData<ExpenseGroup[]>();
+	const expenseGroups = useLoaderData<ExpenseGroup[]>();
 
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      {JSON.stringify(expenseGroups)}
-    </div>
-  );
+	return (
+		<div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+			{JSON.stringify(expenseGroups)}
+		</div>
+	);
 }
