4a158bd8d36573fc7f3aeee3a41de51af5dfd9cc
Santiago Martín Agra <santiagomartinagra@gmail.com>
2022-02-06 00:32:10 +0100

    chore(expense-app): new main title

--- apps/expenses-app/app/routes/index.tsx
+++ apps/expenses-app/app/routes/index.tsx
@@ -1,6 +1,6 @@
 import { InMemoryExpenseGroupRepository } from 'expenses-app-data';
 import { ExpenseGroup, GetAllExpenseGroupsUseCase } from 'expenses-app-domain';
-import { useLoaderData } from 'remix';
+import { MetaFunction, useLoaderData } from 'remix';
 import invariant from 'tiny-invariant';
 
 import { ExpenseGroupList } from '../components/ExpenseGroupList';
@@ -19,8 +19,18 @@ export async function loader(): Promise<ExpenseGroup[]> {
 	return data;
 }
 
+export const meta: MetaFunction = () => {
+	return { title: 'Balbal cash | Comparte gastos como en Blabla car' };
+};
+
 export default function Index() {
 	const expenseGroups = useLoaderData<ExpenseGroup[]>();
 
-	return <ExpenseGroupList expenseGroups={expenseGroups} />;
+	return (
+		<div>
+			<h1>Balbal cash | Comparte gastos como en Blabla car</h1>
+
+			<ExpenseGroupList expenseGroups={expenseGroups} />
+		</div>
+	);
 }
